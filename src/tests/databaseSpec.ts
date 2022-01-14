d994fdbb249d90b033d6be9210708add02c76ce0
Srijan Nayak <srijannayak0@gmail.com>
2022-01-14 16:55:18 +0530

    test: Add try catch block for postgres pool query

--- src/tests/databaseSpec.ts
+++ src/tests/databaseSpec.ts
@@ -3,10 +3,15 @@ import { QueryResult } from "pg";
 
 describe("Postgres pool", (): void => {
   it("should be able to execute simple query", async (): Promise<void> => {
-    const result: QueryResult<{ message: string }> = await pgPool.query(
-      "select $1::text as message",
-      ["test"]
-    );
-    expect(result.rows[0]).toEqual({ message: "test" });
+    try {
+      const result: QueryResult<{ message: string }> = await pgPool.query(
+        "select $1::text as message",
+        ["test"]
+      );
+      expect(result.rows[0]).toEqual({ message: "test" });
+    } catch (error) {
+      console.error("Error: Couldn't execute query successfully");
+      console.error(error);
+    }
   });
 });
