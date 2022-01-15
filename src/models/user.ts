ca107ee9f8667c351b506c7eea911a91fede4ff5
Srijan Nayak <srijannayak0@gmail.com>
2022-01-15 20:06:56 +0530

    fix: Convert shape of stored user before returning data

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,31 +1,47 @@
 import pgPool from "../database";
 import { QueryResult } from "pg";
 
-type passwordDigest = string;
-type passwordPlainText = string;
+type PasswordDigest = string;
+type PasswordPlainText = string;
 
 export type User = {
   id?: number;
   firstName: string;
   lastName: string;
-  password: passwordDigest | passwordPlainText;
+  password: PasswordDigest | PasswordPlainText;
 };
+
+export type StoredUser = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  password_digest: PasswordDigest;
+};
+
+const storedUserToUser = (storedUser: StoredUser): User => ({
+  id: storedUser.id,
+  firstName: storedUser.first_name,
+  lastName: storedUser.last_name,
+  password: storedUser.password_digest,
+});
 
 class UserStore {
   static async index(): Promise<User[]> {
-    const result: QueryResult<User> = await pgPool.query("select * from users");
-    return result.rows;
+    const result: QueryResult<StoredUser> = await pgPool.query(
+      "select * from users"
+    );
+    return result.rows.map(storedUserToUser);
   }
 
   static async show(userId: number): Promise<User> {
-    const result: QueryResult<User> = await pgPool.query(
+    const result: QueryResult<StoredUser> = await pgPool.query(
       "select * from users where id = $1",
       [userId]
     );
     if (!result.rows[0]) {
       throw new Error(`User with ID ${userId} doesn't exist`);
     }
-    return result.rows[0];
+    return storedUserToUser(result.rows[0]);
   }
 }
 
