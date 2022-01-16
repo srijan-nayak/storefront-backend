b79bdfeaf5a57f1601c290b807321be8ebffcb43
Srijan Nayak <srijannayak0@gmail.com>
2022-01-16 13:05:02 +0530

    test: Add tests for UserStore authenticate method

--- src/tests/models/userSpec.ts
+++ src/tests/models/userSpec.ts
@@ -23,6 +23,20 @@ describe("storedUserToUser", () => {
 
 describe("UserStore", (): void => {
   const storedUsers: User[] = [];
+  const sampleUsers: User[] = [
+    {
+      id: "antasia_marjory",
+      firstName: "Antasia",
+      lastName: "Marjory",
+      password: "scenariofallen",
+    },
+    {
+      id: "april_serra",
+      firstName: "April",
+      lastName: "Serra",
+      password: "husbandpope",
+    },
+  ];
 
   describe("index method initially", (): void => {
     it("should return empty result set", async (): Promise<void> => {
@@ -42,15 +56,10 @@ describe("UserStore", (): void => {
 
   describe("create method", (): void => {
     it("should return created user", async (): Promise<void> => {
-      const result: User = await UserStore.create({
-        id: "antasia_marjory",
-        firstName: "Antasia",
-        lastName: "Marjory",
-        password: "scenariofallen",
-      });
-      expect(result.id).toBe("antasia_marjory");
-      expect(result.firstName).toBe("Antasia");
-      expect(result.lastName).toBe("Marjory");
+      const result: User = await UserStore.create(sampleUsers[0]);
+      expect(result.id).toBe(sampleUsers[0].id);
+      expect(result.firstName).toBe(sampleUsers[0].firstName);
+      expect(result.lastName).toBe(sampleUsers[0].lastName);
       expect(typeof result.password).toBe("string");
       storedUsers.push(result);
     });
@@ -58,14 +67,7 @@ describe("UserStore", (): void => {
 
   describe("index method after some data insertion", (): void => {
     it("should return a list of all users", async (): Promise<void> => {
-      storedUsers.push(
-        await UserStore.create({
-          id: "april_serra",
-          firstName: "April",
-          lastName: "Serra",
-          password: "husbandpope",
-        })
-      );
+      storedUsers.push(await UserStore.create(sampleUsers[1]));
       const result: User[] = await UserStore.index();
       expect(result).toEqual(storedUsers);
     });
@@ -82,6 +84,28 @@ describe("UserStore", (): void => {
       const userId = "non_existing_user";
       await expectAsync(UserStore.show(userId)).toBeRejectedWithError(
         `User with ID ${userId} doesn't exist`
+      );
+    });
+  });
+
+  describe("authenticate method", () => {
+    it("should return JWT for correct credentials", async (): Promise<void> => {
+      const result = await UserStore.authenticate(
+        sampleUsers[0].id,
+        sampleUsers[0].password
+      );
+      expect(typeof result).toBe("string");
+    });
+
+    it("should throw error for incorrect credentials", async () => {
+      const incorrectUserId = "non_existing_user";
+      await expectAsync(
+        UserStore.authenticate(incorrectUserId, "random_password")
+      ).toBeRejectedWithError(`User with ID ${incorrectUserId} doesn't exist`);
+      await expectAsync(
+        UserStore.authenticate(sampleUsers[1].id, "wrondpassword")
+      ).toBeRejectedWithError(
+        `Incorrect password for user ${sampleUsers[1].id}`
       );
     });
   });
