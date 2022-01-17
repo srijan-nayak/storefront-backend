d84a26feb88300147a656ba7388c3e791e8b74a4
Srijan Nayak <srijannayak0@gmail.com>
2022-01-17 23:52:02 +0530

    test: Add test for user show handler

--- src/tests/routes/userSpec.ts
+++ src/tests/routes/userSpec.ts
@@ -1,6 +1,6 @@
 import request, { Response } from "supertest";
 import app from "../../index";
-import { User } from "../../models/user";
+import UserStore, { User } from "../../models/user";
 
 describe("GET /user", (): void => {
   it("should return a list of all users", async (): Promise<void> => {
@@ -13,5 +13,24 @@ describe("GET /user", (): void => {
       expect(typeof user.lastName).toBe("string");
       expect(typeof user.password).toBe("string");
     }
+  });
+});
+
+describe("GET /user/:id", (): void => {
+  it("should return details for existing user", async (): Promise<void> => {
+    const existingUser: User = (await UserStore.index())[0];
+    const response: Response = await request(app).get(
+      `/user/${existingUser.id}`
+    );
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(existingUser);
+  });
+
+  it("should return error for non-existing user", async (): Promise<void> => {
+    const response: Response = await request(app).get("/user/some_user");
+    expect(response.status).toBe(404);
+    expect(response.body).toBe(
+      `Error: ${UserStore.errorMessages.UserNotFound}`
+    );
   });
 });
