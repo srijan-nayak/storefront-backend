70a51f580ec6b1a09c75eec7085bd6cc5fac9454
Srijan Nayak <srijannayak0@gmail.com>
2022-01-17 23:53:12 +0530

    test: Check status code in user index handler

--- src/tests/routes/userSpec.ts
+++ src/tests/routes/userSpec.ts
@@ -5,6 +5,7 @@ import UserStore, { User } from "../../models/user";
 describe("GET /user", (): void => {
   it("should return a list of all users", async (): Promise<void> => {
     const response: Response = await request(app).get("/user");
+    expect(response.status).toBe(200);
     const users: User[] = response.body;
     expect(users.length).toBeGreaterThan(1);
     for (const user of users) {
