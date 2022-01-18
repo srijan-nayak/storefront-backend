571397487c36ee7c0ff342019f9edd89f6f694f3
Srijan Nayak <srijannayak0@gmail.com>
2022-01-18 23:06:49 +0530

    test: Add test for user create handler

--- src/tests/routes/userSpec.ts
+++ src/tests/routes/userSpec.ts
@@ -35,3 +35,60 @@ describe("GET /user/:id", (): void => {
     );
   });
 });
+
+describe("POST /user", (): void => {
+  it("should return created user", async (): Promise<void> => {
+    const newUser: User = {
+      id: "lucerito_lazer",
+      firstName: "Lucerito",
+      lastName: "Lazer",
+      password: "mindcontinually",
+    };
+    const response: Response = await request(app).post("/user").send(newUser);
+    const user: User = response.body;
+    expect(user.id).toBe(newUser.id);
+    expect(user.firstName).toBe(newUser.firstName);
+    expect(user.lastName).toBe(newUser.lastName);
+    expect(typeof user.password).toBe("string");
+  });
+
+  it("should return error for duplicate user id", async (): Promise<void> => {
+    const duplicateUser: User = (await UserStore.index())[0];
+    const response: Response = await request(app)
+      .post("/user")
+      .send(duplicateUser);
+    expect(response.status).toBe(409);
+    expect(response.body).toBe(
+      `Error: ${UserStore.errorMessages.UserAlreadyExists}`
+    );
+  });
+
+  it("should return error for invalid user data", async (): Promise<void> => {
+    const invalidUser1: User = {
+      firstName: "Columbus",
+      lastName: "Emma",
+      password: "subsequentlycohen",
+    } as User;
+    const response1: Response = await request(app)
+      .post("/user")
+      .send(invalidUser1);
+    expect(response1.status).toBe(422);
+    expect(response1.body).toBe(
+      `Error: ${UserStore.errorMessages.InvalidFields}`
+    );
+
+    const invalidUser2: User = {
+      id: "nereida_towana",
+      firstName: "Nereida",
+      lastName: "Towana",
+      password: "",
+    };
+    const response2: Response = await request(app)
+      .post("/user")
+      .send(invalidUser2);
+    expect(response2.status).toBe(422);
+    expect(response2.body).toBe(
+      `Error: ${UserStore.errorMessages.InvalidFields}`
+    );
+  });
+});
