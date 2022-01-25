6b8f8698b8b9ec55297207f7aabd02a560c88edc
Srijan Nayak <srijannayak0@gmail.com>
2022-01-25 23:10:11 +0530

    refactor: Cleanup ProductStore index method and remove JSDoc

--- src/models/product.ts
+++ src/models/product.ts
@@ -11,17 +11,13 @@ export type Product = {
 };
 
 class ProductStore {
-  /**
-   * Gets a list of all products in the database.
-   *
-   * @returns list of all products in database
-   */
   static async index(): Promise<Product[]> {
-    const result: QueryResult<Product> = await pgPool.query(
+    const queryResult: QueryResult<Product> = await pgPool.query(
       `select id, name, price::numeric::double precision, category
        from products`
     );
-    return result.rows;
+    const products: Product[] = queryResult.rows;
+    return products;
   }
 
   /**
