445e16a9baa80113d231f0dc5a576bec2f5209e4
Srijan Nayak <srijannayak0@gmail.com>
2022-01-26 19:38:30 +0530

    feat: Implement product show handler

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
 import ProductStore, { Product } from "../models/product";
+import { Result } from "../result";
 
 const productHandler: Router = Router();
 
@@ -9,6 +10,25 @@ productHandler.get(
     try {
       const products: Product[] = await ProductStore.index();
       res.json(products);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+productHandler.get(
+  "/:id",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const productId: number = parseInt(req.params["id"]);
+      const showResult: Result<Product> = await ProductStore.show(productId);
+      if (!showResult.ok) {
+        const error: Error = showResult.data;
+        res.status(404).json(error.toString());
+        return;
+      }
+      const product: Product = showResult.data;
+      res.json(product);
     } catch (error) {
       next(error);
     }
