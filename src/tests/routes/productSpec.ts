239f24d89b10cb7ef101c978ca3e8aaa62ab14e6
Srijan Nayak <srijannayak0@gmail.com>
2022-01-26 19:39:00 +0530

    test: Add test for product show handler

--- src/tests/routes/productSpec.ts
+++ src/tests/routes/productSpec.ts
@@ -1,6 +1,7 @@
 import request, { Response } from "supertest";
 import app from "../../index";
 import { Product } from "../../models/product";
+import { ProductNotFoundError } from "../../errors";
 
 describe("Product handler endpoint", (): void => {
   describe("GET /product", (): void => {
@@ -15,6 +16,24 @@ describe("Product handler endpoint", (): void => {
         expect(typeof product.price).toBe("number");
         expect(typeof product.category).toBe("string");
       }
+    });
+  });
+
+  describe("GET /product/:id", (): void => {
+    it("should return details for existing product", async (): Promise<void> => {
+      const response: Response = await request(app).get("/product/103");
+      expect(response.status).toBe(200);
+      const product: Product = response.body;
+      expect(product.id).toBe(103);
+      expect(product.name).toBe("Sleek Frozen Chair");
+      expect(product.price).toBe(38.34);
+      expect(product.category).toBe("Furniture");
+    });
+
+    it("should return error for non-existing product", async (): Promise<void> => {
+      const response: Response = await request(app).get("/product/182");
+      expect(response.status).toBe(404);
+      expect(response.body).toBe(ProductNotFoundError.toString());
     });
   });
 });
