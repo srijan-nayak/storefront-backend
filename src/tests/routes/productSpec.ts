3c668253bb08ae62cec122821b603ec069f1b421
Srijan Nayak <srijannayak0@gmail.com>
2022-02-04 19:15:45 +0530

    test: Add test for popular products endpoint

--- src/tests/routes/productSpec.ts
+++ src/tests/routes/productSpec.ts
@@ -25,6 +25,23 @@ describe("Product handler endpoint", (): void => {
     });
   });
 
+  describe("GET /products?popular=true", (): void => {
+    it("should return a list of 5 products", async (): Promise<void> => {
+      const response: Response = await request(app)
+        .get("/products")
+        .query({ popular: true });
+      expect(response.status).toBe(200);
+      const products: Product[] = response.body;
+      expect(products.length).toBe(5);
+      for (const product of products) {
+        expect(typeof product.id).toBe("number");
+        expect(typeof product.name).toBe("string");
+        expect(typeof product.price).toBe("number");
+        expect(typeof product.category).toBe("string");
+      }
+    });
+  });
+
   describe("GET /products/:productId", (): void => {
     it("should return details for existing product", async (): Promise<void> => {
       const response: Response = await request(app).get("/products/103");
